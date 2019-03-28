95dc00ed83256b43a3fc5f020de11880c252ce27
NateSopko798 <nksopko@buffalo.edu>
2019-03-27 22:20:34 -0400

    * Changes to controller to start using procedure call

--- PalletBuildAPI/Controllers/PalletBuildController.cs
+++ PalletBuildAPI/Controllers/PalletBuildController.cs
@@ -1,4 +1,5 @@
-﻿using PalletBuildAPI.Models;
+﻿using Newtonsoft.Json;
+using PalletBuildAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -17,75 +18,23 @@ namespace PalletBuildAPI.Controllers
         [HttpPost, Route("CheckInUser")]
         public string CheckInUser([FromBody] StringModel value)
         {
-            //using (SqlConnection con = new SqlConnection("Source=mti-dbs2;Initial Catalog=MEDW;User ID=scanner;Password=scanner"))
-            //using (SqlConnection con = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;"))
-            //{
-            //    using (SqlCommand cmd = new SqlCommand("MTI.GET_BADGE_DATA", con))
-            //    {
-            //        cmd.CommandType = CommandType.StoredProcedure;
-
-            //        cmd.Parameters.Add("@Badgeno", SqlDbType.VarChar).Value = value.stringValue;
-
-            //        DataTable dataTable = new DataTable();
-
-            //        dataTable.Load(cmd.ExecuteReader());
-
-            //        con.Open();
-            //        cmd.ExecuteNonQuery();
-
-            //        var output = new StringBuilder();
-
-            //        var columnsWidths = new int[dataTable.Columns.Count];
-
-            //        // Get column widths
-            //        foreach (DataRow row in dataTable.Rows)
-            //        {
-            //            for (int i = 0; i < dataTable.Columns.Count; i++)
-            //            {
-            //                var length = row[i].ToString().Length;
-            //                if (columnsWidths[i] < length)
-            //                    columnsWidths[i] = length;
-            //            }
-            //        }
-
-            //        // Get Column Titles
-            //        for (int i = 0; i < dataTable.Columns.Count; i++)
-            //        {
-            //            var length = dataTable.Columns[i].ColumnName.Length;
-            //            if (columnsWidths[i] < length)
-            //                columnsWidths[i] = length;
-            //        }
-
-            //        // Write Column titles
-            //        for (int i = 0; i < dataTable.Columns.Count; i++)
-            //        {
-            //            var text = dataTable.Columns[i].ColumnName;
-            //            output.Append("|" + PadCenter(text, columnsWidths[i] + 2));
-            //        }
-            //        output.Append("|\n" + new string('=', output.Length) + "\n");
-
-            //        // Write Rows
-            //        foreach (DataRow row in dataTable.Rows)
-            //        {
-            //            for (int i = 0; i < dataTable.Columns.Count; i++)
-            //            {
-            //                var text = row[i].ToString();
-            //                output.Append("|" + PadCenter(text, columnsWidths[i] + 2));
-            //            }
-            //            output.Append("|\n");
-            //        }
-            //        return output.ToString();
-            //    }
-            //}
+            using(SqlConnection con = new SqlConnection("Server=mti-dbs2;Initial Catalog=MEDW;User ID=scanner;Password=scanner"))
+            {
+                using (SqlCommand cmd = new SqlCommand("MTI.GET_BADGE_DATA", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@BadgeNo", SqlDbType.VarChar).Value = value.stringValue;
+                    cmd.Parameters.Add("@isIndia", SqlDbType.Int).Value = 0;
+                    con.Open();
+                    DataTable dt = new DataTable();
+                    dt.Load(cmd.ExecuteReader());
+                    con.Close();
+                    return JsonConvert.SerializeObject(dt);
+                }
+            }
+            
             return value.stringValue;
         }
-
-        //private static string PadCenter(string text, int maxLength)
-        //{
-        //    int diff = maxLength - text.Length;
-        //    return new string(' ', diff / 2) + text + new string(' ', (int)(diff / 2.0 + 0.5));
-
-        //}
 
         [HttpPost, Route("CheckPalletData")]
         public string CheckPalletData([FromBody] PalletDataModel value)
